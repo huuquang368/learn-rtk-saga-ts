e549930bfee70feca2fa940a605795520f72201f
Quang <huuquang368@gmail.com>
2021-10-22 14:14:20 +0700

    init saga

---
+++ src/features/counter/counterSaga.ts
@@ -0,0 +1,14 @@
+import { delay, put, takeEvery, takeLatest } from '@redux-saga/core/effects';
+import { PayloadAction } from '@reduxjs/toolkit';
+import { incrementSaga, incrementSagaSuccess } from './counterSlice';
+
+function* handleIncrementSaga(action: PayloadAction<number>) {
+  yield delay(1000);
+  console.log('wating done...');
+  yield put(incrementSagaSuccess(action.payload));
+}
+export default function* counterSaga() {
+  console.log('counter saga');
+  // yield takeEvery(incrementSaga.toString(), handleIncrementSaga);
+  yield takeLatest(incrementSaga.toString(), handleIncrementSaga);
+}
